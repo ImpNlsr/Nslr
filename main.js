6954d3ce5992d441eb9c173ca11c2ec8dd29e864
ImpNlsr <58883711+ImpNlsr@users.noreply.github.com>
2019-12-14 19:44:38 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 const client = new Discord.Client();
 
-const BOT_TOKEN = 'NjU1NDI1NzYxNDczMzMxMjIw.XfUmVQ.B5jp0fPjwAZdMVbxnS9LUzV12FQ';
+const BOT_TOKEN = 'NjU1NDI1NzYxNDczMzMxMjIw.XfUsdw.L6-3SmbuiQPAI75qNOxERT90jUY';
 const mcCommand = '!statut'; // Command for triggering
 const mcIP = 'mc.dieperink-hosting.ch'; // MC server IP or hostname address
 const mcPort = 25566; // MC server port (default=25565)
